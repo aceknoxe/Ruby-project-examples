aa32ff974b929320fe184bcb4667aea1e7ba0e71
Harigovind <153193526+harigovindes@users.noreply.github.com>
2024-04-18 17:23:42 +0530

    Update Fibonacci sequence.rb

--- Beginner/Fibonacci sequence.rb
+++ Beginner/Fibonacci sequence.rb
@@ -1,1 +1,19 @@
+def fibonacci(n)
+  a = 0
+  b = 1
+
+  n.times do
+    temp = a
+    a = b
+    b = temp + b
+  end
+
+  return a
+end
+
+# Print the first 10 numbers in the Fibonacci sequence
+10.times do |n|
+  result = fibonacci(n)
+  puts result
+end
 
